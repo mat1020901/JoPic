83162cebe610217eafdd656f697acc5556dd4d0c
mat1020901 <hewmatt201260@gmail.com>
2026-04-02 16:02:56 +0800

    Add new image path for Cheryl

--- media.js
+++ media.js
@@ -76,6 +76,7 @@ const mediaSeries = {
     './images/cheryl/Cheryl-73.jpg',
     './images/cheryl/Cheryl-74.jpg',
     './images/cheryl/Cheryl-75.jpg',
+    './images/cheryl/Cheryl-76.jpg',
   ],
   ellen:[
     './images/ellen/ellen-1.jpg',
